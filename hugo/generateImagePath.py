0cb893073f69a0dbd7a24df3d340f3b292b50183
JohnnyHao <johnny173862903@gmail.com>
2021-06-06 11:40:13 +0800

    hugo格式化工具

---
+++ hugo/generateImagePath.py
@@ -0,0 +1,36 @@
+# 生成hugo图片路径
+
+import os
+import re
+
+
+def main():
+    find_files("/Users/wuhao02/Nutstore Files/文档/JBlog/content/posts/")
+
+
+def find_files(rootpath):
+    for root, dirs, files in os.walk(rootpath):
+        for file in files:
+            if os.path.splitext(file)[-1] == ".md":
+                generate_image_path(os.path.join(root, file))
+
+
+def generate_image_path(filename):
+    print(filename)
+    file = open(filename, mode='r', encoding='utf-8')
+    line = []
+    for txt in file:
+        if re.match("!\[]\(/image/.*?\.png\)", txt):
+            line = line[0: -1]
+        else:
+            if re.match("!\[]\((\.\./)*static/image/.*?\.png\)", txt):
+                line.extend([re.sub("(\.\./)*static/image/", "/image/", txt), '\n'])
+            line.append(txt)
+
+    file = open(filename, mode='w', encoding='utf-8')
+    for txt in line:
+        file.write(txt)
+
+
+if __name__ == "__main__":
+    main()
---
+++ hugo/generateTagsAndCategories.py
@@ -0,0 +1,39 @@
+# 生成hugo标签及分类
+
+import os
+import re
+
+
+def main():
+    find_files("/Users/wuhao02/Nutstore Files/文档/JBlog/content/posts/")
+
+
+def find_files(rootpath):
+    for root, dirs, files in os.walk(rootpath):
+        for file in files:
+            if os.path.splitext(file)[-1] == ".md":
+                filename = os.path.join(root, file)
+                tags = [tag.split(".")[-1] for tag in re.sub(rootpath, "", root).split("/")]
+                categories = '_'.join(tags)
+                generate_tags_and_categories(filename, str(tags), str([categories]))
+
+
+def generate_tags_and_categories(filename, tags, categories):
+    print(filename, tags, categories)
+    file = open(filename, mode='r', encoding='utf-8')
+    line = []
+    for txt in file:
+        if re.match("tags: \[.*?]", txt):
+            line.extend(["tags: ", tags, '\n'])
+        elif re.match("categories: \[.*?]", txt):
+            line.extend(["categories: ", categories, '\n'])
+        else:
+            line.append(txt)
+
+    file = open(filename, mode='w', encoding='utf-8')
+    for txt in line:
+        file.write(txt)
+
+
+if __name__ == "__main__":
+    main()
