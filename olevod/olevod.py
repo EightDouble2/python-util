d0aaf2de9491c715b6e112a3574baddf65270a50
JohnnyHao <johnny173862903@gmail.com>
2021-06-22 15:24:30 +0800

    olevod传参优化

--- olevod/olevod.py
+++ olevod/olevod.py
@@ -1,6 +1,7 @@
 import getHtml
 import getVideo
 import re
+import sys
 
 
 def __main__(root_url, path, video_root_path):
@@ -9,11 +10,8 @@ def __main__(root_url, path, video_root_path):
 
 
 if __name__ == "__main__":
-    input_url = input("请输入视频地址: ")
+    input_url = sys.argv[1]
+    input_video_root_path = sys.argv[2]
     input_root_url = re.findall(r"(.*://.*?)/", input_url)[0]
     input_path = input_url.replace(input_root_url, "")
-    default_root_path = "/Users/wuhao02/Downloads"
-    input_video_root_path = input("请输入保存地址(默认:{}): ".format(default_root_path))
-    if len(input_video_root_path) == 0:
-        input_video_root_path = default_root_path
     __main__(input_root_url, input_path, input_video_root_path)
